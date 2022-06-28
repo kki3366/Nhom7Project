85df82829f636ad0dd2699bfcedbaae6155dae9f
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-06-28 19:17:23 +0700

    up tổng

--- ProjectNhom7/src/com/abc.java
+++ ProjectNhom7/src/com/abc.java
@@ -4,5 +4,9 @@ public class abc {
 	public static void main(String[] args) {
 		System.out.println("Đây là khoa");
 		System.out.println("Đây là Trung");
+		int a = 10;
+		int b = 5;
+		int tong = a +b;
+		System.err.println(tong);
 	}
 }
